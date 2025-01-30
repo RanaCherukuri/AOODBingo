554c0ab1848183ee5059e85ae8a48a0569793976
RanaCherukuri <rcheru5712@gmail.com>
2025-01-30 00:10:17 -0500

    `Refactor random number generator to use array shuffling and pop`

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-let numsCalled = [];//B: 1-15, I:16-30, N: 31-45, G: 46-60 O: 61-75
-let number;
-for(let i = 0; i < 100; i++) {
-    number = Math.floor(Math.random()*75);
-    if (!isCalled(number)) {
-        numsCalled.push(number);
-        console.log(number);
-    }
-    numsCalled.push(number);
+//B: 1-15, I:16-30, N: 31-45, G: 46-60 O: 61-75
+let callOrder = shuffle(newPopulatedArray());
+
+while (callOrder.length > 0) {
+    console.log(numToString(callNumber()));
 }
-console.log("\n");
-console.log(numsCalled);
 
-
-function isCalled(number) {
-    for (let i = 0; i < numsCalled.length; i++) {
-        if (numsCalled[i] == number) {
-            return true;
-        }
+function newPopulatedArray() { // returns an int array [1-75] incl, sorted
+    let ret = [];
+    for (let i = 1; i <= 75; i++) {
+        ret.push(i);
     }
-    return false;
+    return ret;
+}
+
+function shuffle(array) { // shuffles the given array reference
+    for (let i = 0; i < array.length - 1; i++) {
+        let otherPos = Math.floor(Math.random() * (75 - i) + i);
+        let temp = array[i];
+        array[i] = array[otherPos];
+        array[otherPos] = temp;
+    }
+}
+
+function callNumber() {
+    return callOrder.pop();
+}
+
+function numToString(num) {
+    let cat = Math.floor((num - 1) / 15);
+    return (cat > 2 ? (cat > 3 ? "O" : "G") : (cat > 1 ? "N" : (cat > 0 ? "I" : "B"))) + num;
 }
